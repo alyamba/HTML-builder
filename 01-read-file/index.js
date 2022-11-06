4cc16ce50d46d8a6cb625075fda02c42b83a2e7f
Alina <logunionok@gmail.com>
2022-11-06 22:16:55 +0300

    feat: edit functional for 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,11 @@
-let fs = require('fs');
-const path = require('path');
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', function(error, fileContent){
-   if(error) throw error; // ошибка чтения файла, если есть
-   console.log(fileContent); // содержимое файла
+const fs = require("fs");
+const path = require("path");
+
+let readableStream = fs.createReadStream(
+  path.join(__dirname, "text.txt"),
+  "utf8"
+);
+
+readableStream.on("data", function (chunk) {
+  console.log(chunk);
 });
