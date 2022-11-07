5c263a89500d6e2694043cf15e75d96693ccbdc8
Alina <logunionok@gmail.com>
2022-11-08 00:25:42 +0300

    feat: add functionality for copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,13 @@
+const fs = require("fs");
+const path = require("path");
 
+fs.mkdir(path.join(__dirname, "files-copy"), () => {});
+
+fs.promises.readdir(path.join(__dirname, "files")).then((files) =>
+  files.forEach((file) => {
+    fs.promises.copyFile(
+      path.join(__dirname, "files", file),
+      path.join(__dirname, "files-copy", file)
+    );
+  })
+);
