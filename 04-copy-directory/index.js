7566b0c1d51c8716288b48ca8db374617cbdd27a
Alina <logunionok@gmail.com>
2022-11-10 16:01:28 +0300

    fix: edit functionality

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,31 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(path.join(__dirname, "files-copy"), () => {});
+const copy = () => {
+  fs.readdir(path.join(__dirname, "files-copy"), (err, files) => {
+    if (err) throw err;
 
-fs.promises.readdir(path.join(__dirname, "files")).then((files) =>
-  files.forEach((file) => {
-    fs.promises.copyFile(
-      path.join(__dirname, "files", file),
-      path.join(__dirname, "files-copy", file)
-    );
-  })
-);
+    for (const file of files) {
+      fs.unlink(path.join(path.join(__dirname, "files-copy"), file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+
+  fs.mkdir(
+    path.join(__dirname, "files-copy"),
+    { recursive: true, force: true },
+    () => {}
+  );
+
+  fs.promises.readdir(path.join(__dirname, "files")).then((files) =>
+    files.forEach((file) => {
+      fs.promises.copyFile(
+        path.join(__dirname, "files", file),
+        path.join(__dirname, "files-copy", file)
+      );
+    })
+  );
+};
+
+copy();
